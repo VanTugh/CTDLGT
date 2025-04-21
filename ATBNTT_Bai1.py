8b62717c8213acd43118d10d1161f7a4c22ed1ff
VanTugh <nguyenvantung07032005@gmail.com>
2025-04-21 17:00:20 +0700

    on tap nhe

--- ATBNTT_Bai1.py
+++ ATBNTT_Bai1.py
@@ -3,11 +3,7 @@ def ucl(a , b) :
     while b:
         a , b = b, a%b
     return a
-
-
-
-
-a = int(input(" nhap a de : "))
+a= int(input(" nhap a de : "))
 b= int(input(" nhap b de : "))
 print(" uoc so chung lon nhat cua : ",str(a)," va ",str(b) ," la : " + str(ucl(a,b)))
 if ucl(a,b) == 1 :
@@ -16,16 +12,25 @@ else :
     print(" khong phai 2 so nguyen to cung nhau ")
 # tim phan tu nghich dao bang o clit mo rong 
 def nghich_dao(m, n):
-    if ucl(m,n) != 1 :
-        print(" khong phai so nguyen to cung nhau ")
-        return
-    else :
-        if n == 0 :
-            return m, 1, 0
-        else :
-            ucl, m1, n1 = ucl(n,m%n)
-            m= n1
-            n= m1 - (m//n)*n1
-            return ucl, m, n
+    if ucl(m, n) != 1:
+        print("Không phải số nguyên tố cùng nhau")
+        return None
+
+    def extended_gcd(a, b):
+        if b == 0:
+            return a, 1, 0
+        else:
+            gcd, x1, y1 = extended_gcd(b, a % b)
+            x = y1
+            y = x1 - (a // b) * y1
+            return gcd, x, y
+
+    gcd, x, y = extended_gcd(m, n)
+    return x % n  # lấy mod n để đảm bảo nghịch đảo dương
+
+
+m=int(input(" nhap vao m : "))
+n=int(input(" nhap vao n : "))
+print(str(nghich_dao(m,n)) + " la nghich dao cua "+str(m) + " va " +str(n) )
 
 
---
+++ ATBNTT_ONTAP.py
@@ -0,0 +1,57 @@
+# thuat toan tim uoc so chung lon nhat
+def UCL(a,b):
+    while b:
+        a,b=b,a%b
+    return a
+
+#test thuat toan
+a=int(input(" nhap vao a : "))
+b=int(input(" nhap vao b : "))
+print(str(UCL(a,b)) + " la ucl cua "+str(a) + " va " +str(b) )
+# thuat toan tim module nghich dao theo oclit
+def nghich_dao_modulo(a,b):
+    if UCL(a,b)!=1: 
+        print(" hai so nay khong phai la 2 so nguyen to ")
+        return None
+    def extened_gcd(a,b):
+        if b==0:
+            return a,1,0
+        else:
+            gcd,x1,y1=extened_gcd(b,a%b)
+            x=y1
+            y=x1-(a//b)*y1
+            return gcd,x,y
+    gcd, x, y= extened_gcd(a,b)
+    return x%b
+m=int(input(" nhap vao m : "))
+n=int(input(" nhap vao n : "))
+print(str(nghich_dao_modulo(m,n)) + " la nghich dao cua "+str(m) + " va " +str(n) )
+# thuat toan tim module nghich dao theo oclit mo rong
+def ex_clitt(a,b):
+    if UCL(a,b)!=1:
+        return
+    else:
+        r0,r1=a,b
+        x0,x1=0,1
+        y0,y1=1,0
+        while r1 !=1:
+            q=r0//r1
+            r0,r1=r1, r0 - q * r1
+            x0,x1=x1, x1-x0*q
+            y0,y1=y1, y1-y0*q
+        if x1<0:
+            x1=a+x1
+    return x1
+# thuat toan a mod b theo binary
+def binary_T(a,b,n):
+    f=1
+    a=a%n
+    b_bin=bin(b)[2:] #Dùng để cắt chuỗi từ ký tự thứ 2 trở đi (bỏ '0b').
+    for bit in b_bin:
+        f=(f*f)%n
+        if bit == '1':
+            f=f*a
+    return f
+# test
+print(binary_T(7,560,561))
+
